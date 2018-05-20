8c68e3d8da635e6e5350bf02744d17e534d199c5
satori9 <b1806ao@gmail.com>
2018-05-20 11:06:48 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,22 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 永続セッションとしてユーザーを記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
+  # 記憶トークン (cookie) に対応するユーザーを返す
+  def current_user
+  end
+  
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -20,4 +36,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end  
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
