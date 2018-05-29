1ac60fda359be7145675342bdfb7a1b0b03aaf02
satori9 <b1806ao@gmail.com>
2018-05-29 12:31:47 +0000

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,100 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
- before_action :correct_user,   only: [:edit, :update]
- before_action :admin_user,     only: :destroy
+  before_action :logged_in_user, only: [:index, :edit,
+                                        :update, :destroy,
+                                        :following, :followers]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
+    #@users = User.all
     @users = User.paginate(page: params[:page])
   end
-  
+
   # GET /users/:id
   def show
-    @user = User.find(params[:id])
+    @user       = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
+    # => app/views/users/show.html.erb
+    # debugger
   end
 
+  # GET /users/new
   def new
     @user = User.new
+    # => form_for @user
   end
   
   # POST /users
   def create
-    @user = User.new(user_params)
-    if @user.save # => Validation
+     @user = User.new(user_params)
+    if @user.save
       log_in @user
-      # Sucess
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      # GET "/users/#{@user.id}" => show
     else
       # Failure
       render 'new'      
     end
   end
 
-   def edit
+  # GET /users/:id/edit
+  # params[:id] => :id
+  def edit
     @user = User.find(params[:id])
-   end
-   
+    # => app/views/users/edit.html.erb
+  end
+  
+  #PATCH /users/:id
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功した場合を扱う。
+      # Success
       flash[:success] = "Profile updated"
       redirect_to @user
     else
+      # Failure
+      # => @user.errors.full_messages()
       render 'edit'
     end
   end
-
+  
+  # DELETE /users/:id
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
   private
- 
+
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(
+        :name, :email, :password, 
+        :password_confirmation)
     end
-
-    # beforeフィルター
-
-    # 正しいユーザーかどうかを確認
+    
+    # 正しいユーザーかどうか確認
     def correct_user
+      # GET   /users/:id/edit
+      # PATCH /users/:id
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-
-    # 管理者かどうかを確認
+    
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
